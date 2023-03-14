58d13d065d3315499c03e7e6bab3ff9d5f5eda61
sanchesrfl <sanchesrfl@gmail.com>
2023-03-13 22:44:35 -0300

    first commit

---
+++ web-server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Post request foi bem sucedido!\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Nome = %s\n", name)
+	fmt.Fprintf(w, "Cidade = %s\n", address)
+
+}
+
+//funcao principal do script
+
+func main() {
+
+	//servidor de arquivos estáticos do webserver
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	//root handle
+	http.Handle("/", fileServer)
+
+	//form handle
+	http.HandleFunc("/form", formHandler)
+
+	fmt.Printf("Iniciando web-servidor, na porta 8080.\n")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+
+		log.Fatal(err)
+
+	}
+
+}
